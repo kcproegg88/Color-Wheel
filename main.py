626c0adbf0f9d3eb6f39e0dba8b3c6494ec047f1
krishna <krischauhan01@gmail.com>
2024-02-28 16:41:11 -0500

    changed fps

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ pygame.init()
 WIDTH, HEIGHT = 750, 750
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 WINDOW.fill((0, 0, 0))
-FPS = 1
+FPS = 60
 
 
 def init():
@@ -35,38 +35,36 @@ def initLine():
 
 def initRing():
     global ring_list
-    circle_num, radius, origin, dark, angle_rate, distance, width, angle = 20, 5, (WIDTH/2, HEIGHT/2), True, 1, 150, 150, 0
+    circle_num, radius, origin, dark, angle_rate, distance, width, angle = 1, 5, (WIDTH/2, HEIGHT/2), True, 1, 150, 150, 0
     ring_list = [colored_shape.Ring(circle_num, radius, origin, not dark, angle_rate, distance, width, angle, period=math.pi*2)]
     ring_list.append(colored_shape.Ring(circle_num, radius, origin, dark, angle_rate, distance, -width, angle, period=math.pi*2))
 
 
 def update():
     global circle_list, line_list, ring_list
-    ring_list[0].circle_num += 0
+    ring_list[0].circle_num += 1
     ring_list[1].circle_num += 0
     line_list[0].angle += math.pi/180 * 1
 
 
 def main():
-    counter = 0
     running = True
 
     init()
+    clock = pygame.time.Clock()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        if counter % 1 == 0:
-            update()
+        update()
 
         WINDOW.fill(BLACK)
         draw_colored_shape.draw(ring_list, [], [], WINDOW)
         pygame.draw.circle(WINDOW, WHITE, ring_list[0].origin, ring_list[0].radius)
 
         pygame.display.flip()  # Needed to update the screen
-        counter += 1
-
+        clock.tick(FPS)
     pygame.quit()
 
 
